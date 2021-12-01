d6cbde6c8b47b9ffba4d3c72be261b9344fca8ea
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2021-12-01 22:28:13 +0300

    a

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("kemal", 2000.0, 45, 1985);
+        employee.tax();
+        employee.bonus();
+        employee.raiseSalary();
+        employee.personalBilgi();
+    }
+}
